fbf0e175159eb0d6aaba01d360e2968d9e20d142
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-07-26 10:04:18 -0400

    Added functions

--- js/site.js
+++ js/site.js
@@ -1,13 +1,39 @@
-function displayMessage() {
-  let inputBox = document.getElementById("message");
+function getValues() {
+  let userString = document.getElementById("userString").value;
 
-  let message = inputBox.value;
+  let revString = checkForPalindrome(userString);
+  displayResults(revString, userString);
 
-  Swal.fire({
-    backdrop: false,
-    title: "App Name",
-    text: message,
-  });
+  displayResults(revString);
 }
 
+function checkForPalindrome(input, output) {
+  // Use regex to eliminate punctuation and spaces
+  let userString = input.replace(/[^a-zA-Z0-9]/g, "");
 
+  // Ignore Capitalization
+  userString = userString.toLowerCase();
+
+  // Check if the string is a palindrome
+  return userString === userString.split("").reverse().join("");
+}
+
+function displayResults(output, input) {
+  let alertBox = document.getElementById("alert");
+
+  if (output) {
+    let reversedText = input.split("").reverse().join("");
+
+    document.getElementById("results").textContent = reversedText;
+    alertBox.classList.remove("alert-danger");
+    alertBox.classList.add("alert-success");
+  } else {
+    let reversedText = input.split("").reverse().join("");
+    let h4Element = document.querySelector("h4");
+    h4Element.textContent = "“Oh no! That’s not a palindrome.”";
+    document.getElementById("results").textContent = reversedText;
+    alertBox.classList.remove("alert-success");
+    alertBox.classList.add("alert-danger");
+  }
+  alertBox.classList.remove("invisible");
+}
