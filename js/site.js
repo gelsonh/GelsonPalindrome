149d07ffb485795836c0e2e7ad642075c531f244
Gelson Hernandez <gelsonpf49@hotmail.com>
2024-08-23 00:36:26 -0400

    Updated JavaScript Code

--- js/site.js
+++ js/site.js
@@ -1,31 +1,20 @@
-// function getValues() {
-//   let userString = document.getElementById("userString").value;
-//   let isPalindrome = checkForPalindrome(userString);
-//   displayResults(isPalindrome, userString);
-// }
-
-// function checkForPalindrome(input) {
-//   let userString = input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-//   return userString === userString.split("").reverse().join("");
-// }
-
+// Get user input values and validate them
 function getValues() {
-  let userString = document.getElementById("userString").value;
-  let isPalindrome = checkForPalindrome(userString);
+  const userString = document.getElementById("userString").value;
+  const isPalindrome = checkForPalindrome(userString);
   displayResults(isPalindrome, userString);
 }
 
 const regex = /[^a-z0-9]/gi;
 const checkForPalindrome = (input) => {
-  let userString = input.replaceAll(regex, "").toLowerCase();
+  const userString = input.replaceAll(regex, "").toLowerCase();
   return userString === userString.split("").reverse().join("");
-}
-
+};
 
 function displayResults(isPalindrome, input) {
-  let alertBox = document.getElementById("alert");
-  let h4Element = document.querySelector("h4");
-  let revText = input.split("").reverse().join("");
+  const alertBox = document.getElementById("alert");
+  const h4Element = document.querySelector("h4");
+  const revText = input.split("").reverse().join("");
   document.getElementById("results").textContent = revText;
 
   if (isPalindrome) {
